148e2ddd2ff205c61c5f92fc2e2dd7f974ada3ab
Tim Eller <tseller@gmail.com>
2025-09-09 23:49:32 -0700

    Wrap text

--- app/agraph.py
+++ app/agraph.py
@@ -1,5 +1,6 @@
 from knowledge_graph_service import fetch_knowledge_graph
 from streamlit_agraph import agraph, Node, Edge, Config
+import textwrap
 
 def get_agraph(graph_id: str):
     g = fetch_knowledge_graph(graph_id)
@@ -14,7 +15,7 @@ def get_agraph(graph_id: str):
         nodes.append(
                 Node(
                     id=entity['entity_id'], 
-                    label=entity['entity_names'][0], 
+                    label=_wrap_text(entity['entity_names'][0]), 
                     title=title,
                     size=10,
                     shape='box',
@@ -41,3 +42,16 @@ def get_agraph(graph_id: str):
     return agraph(nodes=nodes, edges=edges, config=config)
 
 
+def _wrap_text(text, width=32):
+  """
+  Adds word wrap to a given string at a specified width.
+
+  Args:
+    text: The input string to be wrapped.
+    width: The maximum width of each line. Defaults to 70 characters.
+
+  Returns:
+    A new string with word wrap applied.
+  """
+  wrapped_text = textwrap.fill(text, width=width)
+  return wrapped_text
